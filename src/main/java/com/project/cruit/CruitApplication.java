ee77aab52d3591b2a17cc03b038bb9a203f6a263
sunbi <minorSunbi@gmail.com>
2022-01-21 14:49:55 +0900

    Initial commit

---
+++ src/main/java/com/project/cruit/CruitApplication.java
@@ -0,0 +1,13 @@
+package com.project.cruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruitApplication.class, args);
+	}
+
+}
