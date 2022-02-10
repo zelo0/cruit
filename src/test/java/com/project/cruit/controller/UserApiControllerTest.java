a96d3ca6091d8226a37ea9dbcf7c12fcfeed5e81
zelo0 <minorSunbi@gmail.com>
2022-02-10 20:19:43 +0900

    불필요한 애노테이션 제거, 두번쨰 테스트 통과

--- src/test/java/com/project/cruit/controller/UserApiControllerTest.java
+++ src/test/java/com/project/cruit/controller/UserApiControllerTest.java
@@ -1,32 +1,20 @@
 package com.project.cruit.controller;
 
 import com.google.gson.Gson;
-import com.project.cruit.entity.User;
 import com.project.cruit.exception.NameExistsException;
 import com.project.cruit.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest(UserApiController.class)
-//@SpringBootTest
-//@AutoConfigureMockMvc
 class UserApiControllerTest {
     @Autowired
     private MockMvc mvc;
@@ -56,7 +44,7 @@ class UserApiControllerTest {
         payload.setName("na");
         payload.setPosition("frontend");
 
-        doThrow(NameExistsException.class).when(userService).join(payload.toUser());
+        doThrow(new NameExistsException()).when(userService).join(payload.toUser());
 
         mvc.perform(post("/api/v1/users").contentType(MediaType.APPLICATION_JSON).content(gson.toJson(payload)))
                 .andExpect(status().is(400))
