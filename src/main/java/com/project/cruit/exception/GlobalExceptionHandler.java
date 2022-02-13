e540f488c54d11f56dfcca9420ea53d3cbe6b545
zelo0 <minorSunbi@gmail.com>
2022-02-14 01:02:10 +0900

    SimpleMessageBody 클래스 활용하여 에러 핸들링

--- src/main/java/com/project/cruit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/cruit/exception/GlobalExceptionHandler.java
@@ -1,19 +1,26 @@
 package com.project.cruit.exception;
 
+import com.project.cruit.dto.SimpleMessageBody;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RegisterException.class)
-    public ResponseEntity<Map<String, String>> handleRegisterException(Exception e) {
-        Map<String, String> responseBody = new HashMap<>();
-        responseBody.put("message", e.getMessage());
-        return ResponseEntity.badRequest().body(responseBody);
+    public ResponseEntity<SimpleMessageBody> handleRegisterException(Exception e) {
+        return ResponseEntity.badRequest().body(new SimpleMessageBody(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<SimpleMessageBody> handleMethodArgumentNotValidException(Exception e) {
+        return ResponseEntity.badRequest().body(new SimpleMessageBody("필드의 경고를 확인 후 수정해주세요"));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<SimpleMessageBody> handleRuntimeException(Exception e) {
+        return ResponseEntity.badRequest().body(new SimpleMessageBody("예기치 못한 분제가 발생했습니다"));
     }
 }
