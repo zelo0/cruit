9f0412c05ccf76dd3c2a1c9746dc3105802b4f37
zelo0 <minorSunbi@gmail.com>
2022-02-22 13:30:17 +0900

    유저 정보 바꾸는 함수 추가

--- src/main/java/com/project/cruit/service/UserService.java
+++ src/main/java/com/project/cruit/service/UserService.java
@@ -1,6 +1,9 @@
 package com.project.cruit.service;
 
+import com.project.cruit.domain.Position;
 import com.project.cruit.domain.User;
+import com.project.cruit.domain.UserStack;
+import com.project.cruit.domain.stack.Stack;
 import com.project.cruit.exception.EmailExistsException;
 import com.project.cruit.exception.NameExistsException;
 import com.project.cruit.repository.UserRepository;
@@ -8,6 +11,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -40,5 +47,47 @@ public class UserService {
         return userRepository.existsByEmail(email);
     }
 
+    @Transactional
+    public String setName(Long userId, String name) {
+        User user = userRepository.findById(userId).get();
+        user.setName(name);
+        return user.getName();
+    }
 
+    @Transactional
+    public Position setPosition(Long userId, String position) {
+        User user = userRepository.findById(userId).get();
+
+        switch (position) {
+            case "FRONTEND":
+                user.setPosition(Position.FRONTEND);
+                break;
+            case "BACKEND":
+                user.setPosition(Position.BACKEND);
+                break;
+            case "DESIGN":
+                user.setPosition(Position.DESIGN);
+                break;
+        }
+        System.out.println("user = " + user.getPosition().toString());
+        return user.getPosition();
+    }
+
+
+    @Transactional
+    public Boolean setCanBeLeader(Long userId, Boolean canBeLeader) {
+        User user = userRepository.findById(userId).get();
+        user.setCanBeLeader(canBeLeader);
+        return user.getCanBeLeader();
+    }
+
+    @Transactional
+    public List<Stack> setMyUserStacks(Long userId, List<Stack> stacks) {
+        User user = userRepository.findById(userId).get();
+        user.getUserStacks().clear();
+        for (Stack stack : stacks) {
+            user.getUserStacks().add(new UserStack(user, stack));
+        }
+        return user.getUserStacks().stream().map(userStack -> userStack.getStack() ).collect(Collectors.toList());
+    }
 }
