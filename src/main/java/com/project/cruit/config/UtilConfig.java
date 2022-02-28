71f3a3da7723c1f5b449410c51f2974d4700578b
zelo0 <minorSunbi@gmail.com>
2022-03-01 01:14:49 +0900

    jackson hibernate5 모듈 사용

--- src/main/java/com/project/cruit/config/UtilConfig.java
+++ src/main/java/com/project/cruit/config/UtilConfig.java
@@ -1,5 +1,6 @@
 package com.project.cruit.config;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,5 +10,12 @@ public class UtilConfig {
     @Bean
     public Gson gson() {
         return new Gson();
+    }
+
+    @Bean
+    public Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+        hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
     }
 }
