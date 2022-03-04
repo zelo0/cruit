5936995b37d8cbdbf194b8304d83e5cee1fae597
zelo0 <minorSunbi@gmail.com>
2022-03-04 15:36:37 +0900

    stack으로 user 찾는 메소드 추가

--- src/main/java/com/project/cruit/repository/UserRepository.java
+++ src/main/java/com/project/cruit/repository/UserRepository.java
@@ -1,8 +1,14 @@
 package com.project.cruit.repository;
 
 import com.project.cruit.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +16,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
 
     User findByEmail(String email);
+
+    /* time check 필요 - Is there any more efficient way? */
+    @Query("select u from User u join u.userStacks us join us.stack s where s.name in :stackFilterList")
+    Page<User> findByStackFilter(@Param("stackFilterList") List<String> stackFilterList, Pageable pageable);
+
 }
