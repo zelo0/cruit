cbfa2174776c5fb55b11ec9993c6269ba9a3407b
zelo0 <minorSunbi@gmail.com>
2022-03-05 22:47:47 +0900

    생성자에서 발생하는 예외, 접급 권한 없을 때 예외 처리 추가

--- src/main/java/com/project/cruit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/cruit/exception/GlobalExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.project.cruit.exception;
 
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.project.cruit.dto.SimpleMessageBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,9 +26,20 @@ public class GlobalExceptionHandler {
     public ResponseEntity<SimpleMessageBody> handleMethodArgumentNotValidException(Exception e) {
         return ResponseEntity.badRequest().body(new SimpleMessageBody("필드의 경고를 확인 후 수정해주세요"));
     }
+    
+    @ExceptionHandler(MismatchedInputException.class)
+    public ResponseEntity<SimpleMessageBody> handleMismatchedInputException(Exception e) {
+        return ResponseEntity.badRequest().body(new SimpleMessageBody("전달된 데이터가 올바르지 않습니다"));
+    }
+
+    @ExceptionHandler(NotPermitException.class)
+    public ResponseEntity<SimpleMessageBody> handleNotPermitException(Exception e) {
+        return new ResponseEntity(new SimpleMessageBody(e.getMessage()), HttpStatus.FORBIDDEN);
+    }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<SimpleMessageBody> handleRuntimeException(Exception e) {
+
         return ResponseEntity.badRequest().body(new SimpleMessageBody("예기치 못한 분제가 발생했습니다"));
     }
 }
