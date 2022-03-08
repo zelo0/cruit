989af104382fbfb2324034b3fb2ebe41d15bb8d5
zelo0 <minorSunbi@gmail.com>
2022-03-08 22:45:57 +0900

    파트에 리더 있는 지 확인하는 메소드 countByPartAndIsLeader() 선언

--- src/main/java/com/project/cruit/repository/UserPartRepository.java
+++ src/main/java/com/project/cruit/repository/UserPartRepository.java
@@ -1,9 +1,11 @@
 package com.project.cruit.repository;
 
 import com.project.cruit.domain.UserPart;
+import com.project.cruit.domain.part.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserPartRepository extends JpaRepository<UserPart, Long> {
+    long countByPartAndIsLeader(Part part, boolean isLeader);
 }
