b4a4e376c13caa0fc56c8d604b946e31e045964f
zelo0 <minorSunbi@gmail.com>
2022-03-09 22:37:21 +0900

    yml이 여러 개라 메인 애플리케이션 코드 변경

--- src/main/java/com/project/cruit/CruitApplication.java
+++ src/main/java/com/project/cruit/CruitApplication.java
@@ -2,6 +2,7 @@ package com.project.cruit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,8 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class CruitApplication {
 
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "classpath:aws.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(CruitApplication.class, args);
+		new SpringApplicationBuilder(CruitApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
 	}
 
 }
