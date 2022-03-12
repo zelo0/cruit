e55908e5571f66c921f5e0b74b92ca87ee82de09
zelo0 <minorSunbi@gmail.com>
2022-03-12 17:24:20 +0900

    파트를 변경하는 서비스 로직 추가

--- src/main/java/com/project/cruit/service/PartService.java
+++ src/main/java/com/project/cruit/service/PartService.java
@@ -1,6 +1,10 @@
 package com.project.cruit.service;
 
-import com.project.cruit.domain.PartStatus;
+import com.project.cruit.domain.PartStack;
+import com.project.cruit.domain.User;
+import com.project.cruit.domain.UserPart;
+import com.project.cruit.domain.stack.Stack;
+import com.project.cruit.domain.status.PartStatus;
 import com.project.cruit.domain.Project;
 import com.project.cruit.domain.part.Part;
 import com.project.cruit.repository.PartRepository;
@@ -15,6 +19,8 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class PartService {
     private final PartRepository partRepository;
+    private final UserService userService;
+    private final UserPartService userPartService;
 
     @Transactional
     public void saveParts(List<Part> partList) {
@@ -38,5 +44,30 @@ public class PartService {
 
     public Part findById(Long id) {
         return partRepository.findById(id).get();
+    }
+
+    @Transactional
+    public void modifyUsingStacks(Part part, List<Stack> stacks) {
+        // 초기화 시킨 뒤 다시 채우기
+        part.getPartStacks().clear();
+        for (Stack stack : stacks) {
+            part.getPartStacks().add(new PartStack(part, stack));
+        }
+    }
+
+    @Transactional
+    public void modifyUsingStatus(Part part, String status) {
+        if (status.equals("RECRUITING")) {
+            part.setStatus(PartStatus.RECRUITING);
+        } else {
+            part.setStatus(PartStatus.COMPLETED);
+        }
+    }
+
+    @Transactional
+    public void deleteMember(Part part, Long memberId) {
+        User member = userService.findById(memberId);
+        UserPart target = userPartService.findByPartAndUser(part, member);
+        part.getUserParts().remove(target);
     }
 }
