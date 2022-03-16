39d463787403714f7ba337f8c261d0ad75dedb9d
zelo0 <minorSunbi@gmail.com>
2022-03-16 15:29:05 +0900

    다양한 곳에서 사용하기 위해 함수의 매개변수 추가

--- src/main/java/com/project/cruit/service/NotificationService.java
+++ src/main/java/com/project/cruit/service/NotificationService.java
@@ -1,13 +1,12 @@
 package com.project.cruit.service;
 
-import com.project.cruit.domain.Proposal;
+import com.project.cruit.domain.proposal.Proposal;
 import com.project.cruit.domain.Question;
 import com.project.cruit.domain.User;
 import com.project.cruit.domain.notification.Notification;
 import com.project.cruit.domain.notification.ProposalNotification;
 import com.project.cruit.domain.notification.QuestionNotification;
 import com.project.cruit.repository.NotificationRepository;
-import com.project.cruit.repository.ProposalRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,14 +31,28 @@ public class NotificationService {
 
     /* proposalNotification 생성해서 insert */
     @Transactional
-    public void createProposalNotification(Proposal proposal) {
+    public void createProposalNotification(Proposal proposal, User subject, String message) {
         // notification을 만들고 등록
-        notificationRepository.save(new ProposalNotification(proposal, proposal.getReceiver(), "프로젝트 제안이 들어왔습니다"));
+        notificationRepository.save(new ProposalNotification(proposal, subject, message));
     }
 
     /* questionNotification 생성해서 insert */
     @Transactional
-    public void createQuestionNotification(Question question) {
-        notificationRepository.save(new QuestionNotification(question, question.getProject().getProposer(), "내가 제안한 프로젝트에 댓글이 달렸습니다"));
+    public void createQuestionNotification(Question question, User subject, String message) {
+        notificationRepository.save(new QuestionNotification(question, subject, message));
+    }
+
+    /* 참조할 id가 없는, 메시지만 있는 notification 생성 후 insert */
+    public void createNonReferenceNotification(User subject, String message) {
+        notificationRepository.save(new Notification(subject, message));
+    }
+
+    public Notification findById(Long id) {
+        return notificationRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public void setRead(Notification notification) {
+        notification.setIsRead(true);
     }
 }
