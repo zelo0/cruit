84ee64664ef4e0edbf9b3f9a72c875583bae0602
zelo0 <minorSunbi@gmail.com>
2022-03-16 15:35:10 +0900

    Proposal 찾는 findAllBySender, findAllByReceiver 함수 추가

--- src/main/java/com/project/cruit/repository/ProposalRepository.java
+++ src/main/java/com/project/cruit/repository/ProposalRepository.java
@@ -1,7 +1,13 @@
 package com.project.cruit.repository;
 
-import com.project.cruit.domain.Proposal;
+import com.project.cruit.domain.User;
+import com.project.cruit.domain.proposal.Proposal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProposalRepository extends JpaRepository<Proposal, Long> {
+    List<Proposal> findAllBySender(User user);
+
+    List<Proposal> findAllByReceiver(User user);
 }
