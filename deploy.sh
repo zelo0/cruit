0e5d4ae46b68014905bd8b43cacd9e7cfabb930b
zelo0 <minorSunbi@gmail.com>
2022-03-21 08:52:06 +0900

    dockerhub의 계정 id여야 함

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 sudo docker ps -a -q --filter "name=cruit" | grep -q . && docker stop cruit && docker rm cruit | true
 
-sudo docker rmi lonely/cruit:1.0
+sudo docker rmi lonelylee/cruit:1.0
 
-sudo docker pull lonely/cruit:1.0
+sudo docker pull lonelylee/cruit:1.0
 
-docker run -d -p 8080:8080 --name cruit lonely/cruit:1.0
+docker run -d -p 8080:8080 --name cruit lonelylee/cruit:1.0
 
 docker rmi -f $(docker image -f "dangling=true" -q) || true
