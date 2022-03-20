1df9d906add158ebbb22466fc29b202df3f6b1e0
zelo0 <minorSunbi@gmail.com>
2022-03-20 20:43:53 +0900

    s3에 업로드하기 전에 썸네일 크기만 필요하므로 image resize

--- src/main/java/com/project/cruit/service/S3UploaderService.java
+++ src/main/java/com/project/cruit/service/S3UploaderService.java
@@ -2,18 +2,23 @@ package com.project.cruit.service;
 
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.project.cruit.util.ImageResizeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 @Service
+@Slf4j
 public class S3UploaderService {
     private final AmazonS3Client amazonS3Client;
+    private final ImageResizeService imageResizeService;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
@@ -22,7 +27,8 @@ public class S3UploaderService {
         String storedFilePath = dirName + "/" + UUID.randomUUID() + file.getOriginalFilename();
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(file.getSize());
-        amazonS3Client.putObject(bucket, storedFilePath, file.getInputStream(), metadata);
+        ByteArrayInputStream resizedStream = imageResizeService.resizeImage(file, 50, 50);
+        amazonS3Client.putObject(bucket, storedFilePath, resizedStream, metadata);
         return amazonS3Client.getUrl(bucket, storedFilePath).toString();
     }
 }
