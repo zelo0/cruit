4f18d1aa81eb71c65363be4602beda64f21bb000
zelo0 <minorSunbi@gmail.com>
2022-03-21 08:49:29 +0900

    deploy 디렉토리명 변경

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 sudo docker ps -a -q --filter "name=cruit" | grep -q . && docker stop cruit && docker rm cruit | true
 
-sudo docker rmi minor/cruit:1.0
+sudo docker rmi lonely/cruit:1.0
 
-sudo docker pull minor/cruit:1.0
+sudo docker pull lonely/cruit:1.0
 
-docker run -d -p 8080:8080 --name cruit minor/cruit:1.0
+docker run -d -p 8080:8080 --name cruit lonely/cruit:1.0
 
 docker rmi -f $(docker image -f "dangling=true" -q) || true
