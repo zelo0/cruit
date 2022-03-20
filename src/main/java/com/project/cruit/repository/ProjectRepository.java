f3409a8c4cb9deaf6ccd70d452aa0e9e73b83d8c
zelo0 <minorSunbi@gmail.com>
2022-03-20 20:44:38 +0900

    프로젝트에 멤버로 참여하고 있는 파트 개수인 isMemberInLong() 추가

--- src/main/java/com/project/cruit/repository/ProjectRepository.java
+++ src/main/java/com/project/cruit/repository/ProjectRepository.java
@@ -1,6 +1,7 @@
 package com.project.cruit.repository;
 
 import com.project.cruit.domain.Project;
+import com.project.cruit.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,6 +18,10 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     @Query("select p from Project p where p.status = 'PUBLIC'")
     Page<Project> findAllPublic(Pageable pageable);
+
+    @Query(value = "select count(*) from (select project_id as pid from Project p where p.project_id = :projectId) join Part pa on (pa.project_id = pid) join User_Part up on (pa.part_id = up.part_id)" +
+            " where up.user_id = :userId", nativeQuery = true)
+    Long isMemberInLong(@Param("projectId") Long projectId, @Param("userId") Long userId);
 
 //    Page<Project> findAllEfficient(Pageable pageable);
 }
