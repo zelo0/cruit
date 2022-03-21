c0434bc32abef0f5349949913cbb9d230f7b57bd
zelo0 <minorSunbi@gmail.com>
2022-03-21 15:16:05 +0900

    application.yml 안에 aws 내용 넣고 applcation.yml을 github에 업로드 안 하면 됨

--- src/main/java/com/project/cruit/CruitApplication.java
+++ src/main/java/com/project/cruit/CruitApplication.java
@@ -10,14 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class CruitApplication {
 
-	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application-dev.yml,"
-			+ "classpath:aws.yml";
-
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(CruitApplication.class)
-				.properties(APPLICATION_LOCATIONS)
-				.run(args);
+		new SpringApplicationBuilder(CruitApplication.class).run(args);
 	}
 
 }
