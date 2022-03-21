2017ebd81738ca5fadc1247378fa7ea2919407f7
zelo0 <minorSunbi@gmail.com>
2022-03-21 20:19:45 +0900

    ImageResizeServiceTest를 위한 이미지 업로드

--- src/test/java/com/project/cruit/util/ImageResizeServiceTest.java
+++ src/test/java/com/project/cruit/util/ImageResizeServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.mock.web.MockMultipartFile;
@@ -22,11 +23,14 @@ class ImageResizeServiceTest {
     @InjectMocks
     private ImageResizeService imageResizeService;
 
+    @Mock
+    private MockMultipartFile multipartFile;
+
     @Test
     @DisplayName("이미지 사이즈 줄이기")
     void resizeImage() throws IOException {
         // given
-        ClassPathResource resource = new ClassPathResource("/static/testcover.png");
+        ClassPathResource resource = new ClassPathResource("/static/test-image.png");
         MultipartFile inputFile = new MockMultipartFile("test.jpg", resource.getInputStream());
 
         // when
