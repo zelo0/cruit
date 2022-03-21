fb596454c6b172b6e6a3dd5973d1cbc26295d697
zelo0 <minorSunbi@gmail.com>
2022-03-21 16:28:33 +0900

    UserStackTest.java 불완전하니 후에 작성

--- src/test/java/com/project/cruit/domain/UserStackTest.java
+++ src/test/java/com/project/cruit/domain/UserStackTest.java
@@ -7,20 +7,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
-class UserStackTest {
-//    @Autowired
-//    private User user;
-
-
-    public void add_stack_in_user_makeUserStack() {
-        User user = new User();
-        user.getUserStacks().add(new UserStack());
-    }
-
-    @Test
-    public void remove_in_user_shouldRemoveInStack() {
-        User user = new User();
-        user.getUserStacks().add(new UserStack());
-    }
-}
+//@SpringBootTest
+//class UserStackTest {
+////    @Autowired
+////    private User user;
+//
+//    @Test
+//    public void add_stack_in_user_makeUserStack() {
+//        User user = new User();
+//        user.getUserStacks().add(new UserStack());
+//    }
+//
+//    @Test
+//    void remove_in_user_shouldRemoveInStack() {
+//        User user = new User();
+//        user.getUserStacks().add(new UserStack());
+//    }
+//}
