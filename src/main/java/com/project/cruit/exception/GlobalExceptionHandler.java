f23a600d2edd6abd84605ced7d38151b005d8789
zelo0 <minorSunbi@gmail.com>
2022-03-25 12:40:51 +0900

    예외 발생 시 예외 메시지를 응답으로 보내지 말기

--- src/main/java/com/project/cruit/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/cruit/exception/GlobalExceptionHandler.java
@@ -20,13 +20,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RegisterException.class)
     public ResponseEntity<SimpleMessageBody> handleRegisterException(Exception e) {
         log.error(e.getMessage());
-        return ResponseEntity.badRequest().body(new SimpleMessageBody(e.getMessage()));
+        return ResponseEntity.badRequest().body(new SimpleMessageBody("회원가입 중 예외가 발생했습니다"));
     }
 
     @ExceptionHandler(NotHaveSessionException.class)
     public ResponseEntity<SimpleMessageBody> handleSessionException(Exception e) {
         log.error(e.getMessage());
-        return new ResponseEntity(new SimpleMessageBody(e.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity(new SimpleMessageBody("로그인돼있지 않습니다"), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -45,7 +45,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotPermitException.class)
     public ResponseEntity<SimpleMessageBody> handleNotPermitException(Exception e) {
         log.error(e.getMessage());
-        return new ResponseEntity(new SimpleMessageBody(e.getMessage()), HttpStatus.FORBIDDEN);
+        return new ResponseEntity(new SimpleMessageBody("권한이 없습니다"), HttpStatus.FORBIDDEN);
     }
 
     // optional 객체에 들은 게 없을 때 get을 호출할 때, ....
@@ -59,14 +59,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AlreadyClosedPartException.class)
     public ResponseEntity<SimpleMessageBody> handleAlreadyClosedPartException(Exception e){
         log.error(e.getMessage());
-        return ResponseEntity.badRequest().body(new SimpleMessageBody(e.getMessage()));
+        return ResponseEntity.badRequest().body(new SimpleMessageBody("이미 닫힌 파트입니다"));
     }
 
     // 유효하지 않은 페이지 검색 시
     @ExceptionHandler(InvalidPageOffsetException.class)
     public ResponseEntity<SimpleMessageBody> handleInvalidPageOffsetException(Exception e){
         log.error(e.getMessage());
-        return ResponseEntity.badRequest().body(new SimpleMessageBody(e.getMessage()));
+        return ResponseEntity.badRequest().body(new SimpleMessageBody("유효하지 않은 페이지이입니다"));
     }
     
     @ExceptionHandler(IOException.class)
