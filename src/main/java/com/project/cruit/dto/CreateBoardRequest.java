770836c51e4a10dac7f5576affd9450fc14f8508
zelo0 <minorSunbi@gmail.com>
2022-06-21 19:42:08 +0900

    builder 추가

--- src/main/java/com/project/cruit/dto/CreateBoardRequest.java
+++ src/main/java/com/project/cruit/dto/CreateBoardRequest.java
@@ -1,12 +1,14 @@
 package com.project.cruit.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
